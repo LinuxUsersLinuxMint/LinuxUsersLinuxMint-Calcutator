51c4cd170093a0778f5d1354f5c50f621c524c6d
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2023-10-08 20:54:20 +0300

    Updated calc.py /EN/

--- EN/calc.py
+++ EN/calc.py
@@ -1,50 +1,76 @@
 #!/usr/bin/python3
-"Copyright© 2023 LinuxUsersLinuxMint"
-"Python Calcutator Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır."
-"Python Calcutator All Rights Reserved under the GPL(General Public License)."
-"Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
-"A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint"
-
-command=input('calc> ')
-about="Python Calcutator CLI(Command Line Interface / Komut Satırı Arayüzü) LICENCE=GPL2"
-
+import os
+from colorama import Fore, init
+init(autoreset=True)
+# Copyright© 2023 LinuxUsersLinuxMint
+# Python Calcutator Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
+# Python Calcutator All Rights Reserved under the GPL(General Public License).
+# Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
+# A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint
+print("******************************************************")
+print("*** Python-Calcutator 0.2.7 Programına Hoşgeldiniz ***")
+print("""*** Seçenekler:                                    ***
+***                                                ***
+*** Seçmek istediğiniz Komutu Giriniz...           ***
+***                                                ***
+*** 1. calc                                        ***
+*** 2. about                                       ***
+*** 3. exit                                        ***
+*** 4. help                                        ***
+*** 5. git-address                                 ***
+*** 6. ver                                         ***
+*** 7. licence                                     ***
+*** 8. Thank                                       ***
+******************************************************""")
+command=str(input(Fore.LIGHTBLUE_EX +'calc> '))
+os.system("clear")
+about="Python Hesap Makinesi CLI(Command Line Interface / Komut Satırı Arayüzü) LICENCE=GPL2"
+secilen_islem=str(Fore.LIGHTBLUE_EX+"None")
 if command=="calc":
-    print("calc> Transactions You Can Enter: ")
-    print("collect\nExtraction\n\Impact\nDivide\nPercentage\nabout")
-    number1=input('{0} Enter The 1st number: '. format(command))
-    number2=input('{0} Enter The 2st number: '. format(command))
-    process=input('{0} Enter the Transaction You Want to Perform: '. format(command))
-    addition=float(number1)+float(number2)
-    subraction=float(number1)-float(number2)
-    multiplication=float(number1)*float(number2)
-    division=float(number1)/float(number2)
-    Percentage=float(number1)%float(number2)
-    if process=="collect": 
-       print("{0} + {1} = {2}". format(number1,number2,addition))  
-    elif process=="Extraction":
-       print("{0} - {1} = {2}". format(number1,number2,subraction))
-    elif process=="Impact":
-       print("{0} * {1} = {2}". format(number1,number2,multiplication))
-    elif process=="Divide":
-       print("{0} / {1} = {2}". format(number1,number2,division))
-    elif process=="Percentage":
-       print("{0} % {1} = {2}". format(number1,number2,Percentage))
-    else:
-       print("Invalid Proccess!")
-if command=="about":
-   print(about)
-elif command=="exit":
-   exit()
-elif command=="help":
-   print("Python calc Help")
-   print("\n Command: calc , about , help , exit , git-address , web-site , ver , licence")
-elif command=="git-address":
-   print("Github Link: https://github.com/LinuxUsersLinuxMint")
-elif command=="web-site":
-   print("linuxuserslinuxmint.github.io")
-elif command=="ver":
-   print("Version: 0.2 (Last Updated September 17 , 2023 , 14:43)")
-elif command=="licence":
-   print("This Software is protected under the GPL2 license")
+   print("""*** Girebileceğiniz işlemler:     ***
+         Seçilen İşlem={0}""". format(secilen_islem))
+   print("""
+1.   Addition
+2.   Subraction
+3.   Multiplication
+4.   Division
+5.   Percentage""")
+   process=str(input('calc> Seçmek İstediğiniz İşlemin Numarasını Giriniz: '))
+   secilen_islem=process
+   os.system("clear")
+   print(Fore.LIGHTBLUE_EX+"""calc> Seçilen İşlem: 
+         Seçilen İşlem={0}""". format(secilen_islem))
+   number1=float(input('calc> Enter The 1st Number: '))
+   number2=float(input('calc> Enter The 2st Number: '))
+   os.system("clear")
+if islem=="1": 
+     print("{0} + {1} = {2}". format(sayi1,sayi2,sayi1+sayi2))  
+elif islem=="2":
+     print("{0} - {1} = {2}". format(sayi1,sayi2,sayi1-sayi2))
+elif islem=="3":
+     print("{0} * {1} = {2}". format(sayi1,sayi2,sayi1*sayi2))
+elif islem=="4":
+     print("{0} / {1} = {2}". format(sayi1,sayi2,sayi1/sayi2))
+elif islem=="5":
+     print("{0} % {1} = {2}". format(sayi1,sayi2,sayi1%sayi2))
 else:
-   print("Invalid Command!")
+     print("Geçersiz İşlem!")
+     if command=="about":
+          print(about)
+     elif command=="exit":
+          exit()
+     elif command=="help":
+          print("Python calc Help")
+          print("\n Command: calc , about , help , exit , git-address , web-site , ver , licence , Thank")
+     elif command=="git-address":
+          print("Github Link: https://github.com/LinuxUsersLinuxMint")
+     elif command=="web-site":
+          print("linuxuserslinuxmint.github.io")
+     elif command=="ver":
+          print("Sürüm: 0.2.7 (Son Güncellenme Tarihi 6 Ekim , 2023 , 18:03)")
+     elif command=="licence":
+          print("This Software is protected under the GPL2 license")
+     elif command=="Thank":
+          print("Thank You for Using Python-Calcutator.")
+     else:
+          print("Invalid Command!")
