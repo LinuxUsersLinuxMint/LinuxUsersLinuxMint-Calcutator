81491c929c39a2ac1030f384ba9fd679b915cf1a
Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>
2024-05-31 23:24:19 +0300

    Important Update.
    Signed-off-by: Umut <143949134+LinuxUsersLinuxMint@users.noreply.github.com>

--- EN/calc.py
+++ EN/calc.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 import os
 from colorama import Fore, init
+from Basic_Maths import *
 init(autoreset=True)
 process=int(0)
-""" Copyright© 2023-2024 LinuxUsersLinuxMint
+error_dialog = "Invalid Process/Command!"
+"""Copyright© 2023-2024 LinuxUsersLinuxMint
 Python Calcutator Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
 Python Calcutator All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/Python-Calcutator
-A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/Python-Calcutator """
-print("******************************************************")
-print("*** Welcome to Python-Calcutator 0.3 Program       ***")
-print("""*** Options:                                       ***
-***                                                ***
-*** Enter the Command you want to choose...        ***
-***                                                ***
-*** 1. calc                                        ***
-*** 2. about                                       ***
-*** 3. exit                                        ***
-*** 4. help                                        ***
-*** 5. git-address                                 ***
-*** 6. ver                                         ***
-*** 7. licence                                     ***
-*** 8. Thank                                       ***
-******************************************************""")
+Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint-Calcutator
+A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint-Calcutator """
+print("*********************************************************************")
+print("*** Welcome to LinuxUsersLinuxMint-Calcutator 0.3.1 Program       ***")
+print("""*** Options:                                                      ***
+***                                                               ***
+*** Enter the Command you want to choose...                       ***
+***                                                               ***
+*** 1. calc                                                       ***
+*** 2. about                                                      ***
+*** 3. exit                                                       ***
+*** 4. help                                                       ***
+*** 5. git-address                                                ***
+*** 6. ver                                                        ***
+*** 7. licence                                                    ***
+*** 8. Thank                                                      ***
+*********************************************************************""")
 command=str(input(Fore.LIGHTBLUE_EX +'calc> '))
 os.system("clear")
 about="Python Calcutator CLI(Command Line Interface) LICENCE=GPL2"
@@ -32,7 +34,7 @@ if command=="calc":
          Selected Transaction={0}""". format(selected_transaction))
    print("""
 1.   Addition
-2.   Subraction
+2.   Extraction
 3.   Multiplication
 4.   Division
 5.   Percentage""")
@@ -45,33 +47,49 @@ if command=="calc":
    number2=float(input('calc> Enter The 2st Number: '))
    os.system("clear")
 if process=="1": 
-     print("{0} + {1} = {2}". format(number1,number2,number1+number2))  
+     addition(number1,number2,"Result: ")
 elif process=="2":
-     print("{0} - {1} = {2}". format(number1,number2,number1-number2))
+     Extraction(number1,number2,"Result: ")
 elif process=="3":
-     print("{0} * {1} = {2}". format(number1,number2,number1*number2))
+     Multiplication(number1,number2,"Result: ")
 elif process=="4":
-     print("{0} / {1} = {2}". format(number1,number2,number1/number2))
+     Division(number1,number2,"Result: ", "Number cannot be zero in division!")
 elif process=="5":
-     print("{0} % {1} = {2}". format(number1,number2,number1%number2))
+     Percentage(number1,number2,"Result: ")
 else:
-     print("Invalid Process!")
-     if command=="about":
-          print(about)
-     elif command=="exit":
+     error_msg()
+
+if command=="about":
+     print(about)
+elif command=="exit":
+     exit_select = input('Select the method to exit the program (0: Dialogue and Time entry, 1: Time entry only, 2: Dialogue entry only, 3: Normal exit (old style)): ')
+     exit_select = int(exit_select)
+     if exit_select == 0:
+          userTime = input('After how many seconds should the program be closed?: ')
+          userTime = int(userTime)
+          exit_program_dialog_time("Exit program...", userTime)
+     elif exit_select == 1:
+          userTime = input('After how many seconds should the program be closed?: ')
+          userTime = int(userTime)
+          exit_program_time(userTime)
+     elif exit_select == 2:
+          exit_program_dialog("Exit program...")
+     elif exit_select == 3:
           exit()
-     elif command=="help":
-          print("Python calc Help")
-          print("\n Command: calc , about , help , exit , git-address , web-site , ver , licence , Thank")
-     elif command=="git-address":
-          print("Github Link: https://github.com/LinuxUsersLinuxMint")
-     elif command=="web-site":
-          print("linuxuserslinuxmint.github.io")
-     elif command=="ver":
-          print("Version: 0.3 (Last Updated on May 8, 2024, 21:54)")
-     elif command=="licence":
-          print("This Software is protected under the GPL2 license")
-     elif command=="Thank":
-          print("Thank You for Using Python-Calcutator.")
      else:
-          print("Invalid Command!")
+          error_msg()
+elif command=="help":
+     print("LinuxUsersLinuxMint-Calcutator Help")
+     print("\n Command: calc , about , help , exit , git-address , web-site , ver , licence , Thank")
+elif command=="git-address":
+     print("Github Link: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint-Calcutator")
+elif command=="web-site":
+     print("https://linuxuserslinuxmint.github.io")
+elif command=="ver":
+     print("Version: 0.3.1 (Last Updated on May 31, 2024, 23:30)")
+elif command=="licence":
+     print("This Software is protected under the GPL2 license")
+elif command=="Thank":
+     print("Thank You for Using Python-Calcutator.")
+else:
+     error_msg()
--- TR/calc.py
+++ TR/calc.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 import os
 from colorama import Fore, init
+from Basic_Maths import *
 init(autoreset=True)
 islem=int(0)
+error_dialog = "Geçersiz İşlem/Komut!"
 """ Copyright© 2023-2024 LinuxUsersLinuxMint
-Python Calcutator Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
-Python Calcutator All Rights Reserved under the GPL(General Public License).
-Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/Python-Calcutator
-A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/Python-Calcutator """
-print("******************************************************")
-print("*** Python-Calcutator 0.3 Programına Hoşgeldiniz   ***")
-print("""*** Seçenekler:                                    ***
-***                                                ***
-*** Seçmek istediğiniz Komutu Giriniz...           ***
-***                                                ***
-*** 1. calc                                        ***
-*** 2. about                                       ***
-*** 3. exit                                        ***
-*** 4. help                                        ***
-*** 5. git-address                                 ***
-*** 6. ver                                         ***
-*** 7. licence                                     ***
-*** 8. Thank                                       ***
-******************************************************""")
+LinuxUsersLinuxMint Calcutator Tüm Hakları GPL(Genel Kamu Lisansı) altında korunmaktadır.
+LinuxUsersLinuxMint Calcutator All Rights Reserved under the GPL(General Public License).
+Bu Yazılımın Bir Kopyası GİTHUB da yayınlanmaktadır Görüntülemek için: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint-Calcutator
+A Copy of This Software is published on GITHUB To view: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint-Calcutator """
+print("*********************************************************************")
+print("*** LinuxUsersLinuxMint-Calcutator 0.3.1 Programına Hoşgeldiniz   ***")
+print("""*** Seçenekler:                                                   ***
+***                                                               ***
+*** Seçmek istediğiniz Komutu Giriniz...                          ***
+***                                                               ***
+*** 1. calc                                                       ***
+*** 2. about                                                      ***                     
+*** 3. exit                                                       ***                      
+*** 4. help                                                       ***
+*** 5. git-address                                                ***                              
+*** 6. ver                                                        ***          
+*** 7. licence                                                    ***                                  
+*** 8. Thank                                                      ***                                    
+*********************************************************************""")
 command=str(input(Fore.LIGHTBLUE_EX +'calc> '))
 os.system("clear")
 about="Python Hesap Makinesi CLI(Komut Satırı Arayüzü) LICENCE=GPL2"
@@ -45,33 +47,48 @@ if command=="calc":
    sayi2=float(input('calc> 2. sayiyi giriniz: '))
    os.system("clear")
 if islem=="1": 
-     print("{0} + {1} = {2}". format(sayi1,sayi2,sayi1+sayi2))  
+     addition(sayi1,sayi2,"Sonuç: ")  
 elif islem=="2":
-     print("{0} - {1} = {2}". format(sayi1,sayi2,sayi1-sayi2))
+     Extraction(sayi1,sayi2,"Sonuç: ")
 elif islem=="3":
-     print("{0} * {1} = {2}". format(sayi1,sayi2,sayi1*sayi2))
+     Multiplication(sayi1,sayi2,"Sonuç: ")
 elif islem=="4":
-     print("{0} / {1} = {2}". format(sayi1,sayi2,sayi1/sayi2))
+     Division(sayi1,sayi2,"Sonuç: ","Bölme işleminde sayılar 0 olamaz!")
 elif islem=="5":
-     print("{0} % {1} = {2}". format(sayi1,sayi2,sayi1%sayi2))
+     Percentage(sayi1,sayi2,"Sonuç: ")
 else:
-     print("Geçersiz İşlem!")
-     if command=="about":
-          print(about)
-     elif command=="exit":
+     error_msg()
+if command=="about":
+     print(about)
+elif command=="exit":
+     exit_select = int(input('Select the method to exit the program (0: Dialogue and Time entry, 1: Time entry only, 2: Dialogue entry only, 3: Normal exit (old style)): '))
+     exit_select = int(exit_select)
+     if exit_select == 0:
+          userTime = input('After how many seconds should the program be closed?: ')
+          userTime = int(userTime)
+          exit_program_dialog_time("Exit program...", userTime)
+     elif exit_select == 1:
+          userTime = input('After how many seconds should the program be closed?: ')
+          userTime = int(userTime)
+          exit_program_time(userTime)
+     elif exit_select == 2:
+          exit_program_dialog("Exit program...")
+     elif exit_select == 3:
           exit()
-     elif command=="help":
-          print("Python calc Help")
-          print("\n Command: calc , about , help , exit , git-address , web-site , ver , licence , Thank")
-     elif command=="git-address":
-          print("Github Link: https://github.com/LinuxUsersLinuxMint")
-     elif command=="web-site":
-          print("linuxuserslinuxmint.github.io")
-     elif command=="ver":
-          print("Sürüm: 0.3 (Son Güncellenme Tarihi 8 Mayıs , 2024 , 21:54)")
-     elif command=="licence":
-          print("Bu Yazılım GPL2 lisansı kapsamında korunmaktadır.")
-     elif command=="Thank":
-          print("Python-Calcutator'u Kullandığınız için Teşekkür ederim. ")
      else:
-          print("Geçersiz Komut!")
+          error_msg()
+elif command=="help":
+     print("Python calc Help")
+     print("\n Command: calc , about , help , exit , git-address , web-site , ver , licence , Thank")
+elif command=="git-address":
+     print("Github Link: https://github.com/LinuxUsersLinuxMint/LinuxUsersLinuxMint-Calcutator")
+elif command=="web-site":
+     print("linuxuserslinuxmint.github.io")
+elif command=="ver":
+     print("Sürüm: 0.3.1 (Son Güncellenme Tarihi 31 Mayıs , 2024 , 23:30)")
+elif command=="licence":
+     print("Bu Yazılım GPL2 lisansı kapsamında korunmaktadır.")
+elif command=="Thank":
+     print("Python-Calcutator'u Kullandığınız için Teşekkür ederim. ")
+else:
+     error_msg()
